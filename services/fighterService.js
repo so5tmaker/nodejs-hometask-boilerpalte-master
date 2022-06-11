96480cd4b85f3956d05a0370b1f8e5ff52e7bad8
Viktor Sivulskiy <info@softmaker.kz>
2022-06-11 15:46:09 +0600

    FighterService was implemented

--- services/fighterService.js
+++ services/fighterService.js
@@ -1,6 +1,25 @@
 const { FighterRepository } = require('../repositories/fighterRepository');
 
 class FighterService {
+    getFighters() {
+        return FighterRepository.getAll();
+    }
+
+    getFighterById(id) {
+        return FighterRepository.getOne({ id });
+    }
+
+    addFighter(data) {
+        return FighterRepository.create(data);
+    }
+
+    updateFighter(id, data) {
+        return FighterRepository.update(id, data);
+    }
+
+    deleteFighter(id) {
+        return FighterRepository.delete(id);
+    }
     // TODO: Implement methods to work with fighters
 }
 
